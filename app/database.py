1333de7fde7823765831c5f440f96bfbfecd6757
skrupulezno <skrupulezno@gmail.com>
2025-02-25 14:56:17 +0900

    sdads

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://admin:123456@localhost:5432/mpit"
+DATABASE_URL = "postgresql://admin:123456@db:5432/mpit"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
