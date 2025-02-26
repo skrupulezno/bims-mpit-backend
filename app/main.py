272d2e8f762dc2fb05e306f47b24b4f873fedb5f
skrupulezno <skrupulezno@gmail.com>
2025-02-26 15:52:27 +0900

    ds

--- app/main.py
+++ app/main.py
@@ -1,11 +1,27 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.database import engine, Base
 from app import models
 from app.routes import auth_routes
 
-Base.metadata.drop_all(bind=engine)
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3002",
+    "http://localhost:8000",  
+    "http://127.0.0.1",
+    "http://138.124.20.90:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins, 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_routes.router)
